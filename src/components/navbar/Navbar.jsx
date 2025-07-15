1dec2f7c4829de195a6a0fb15c37a0eaa28989c0
Sameer_Shams <sumairshams1890@gmail.com>
2025-07-15 18:53:07 +0500

    working on navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css';
+
+function Navbar(){
+    return(
+<nav className="Navbar">
+<div className="Navbar-logo">
+<div className="Navbar-page-link">2
+<div className="Navbar-social-link">3
+ss
+</div>   
+</div>
+</div>
+</nav>
+    );
+}
+
+export default Navbar;
